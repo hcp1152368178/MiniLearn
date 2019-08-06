cbf1f839933fd8e3ffb9b27d94f892a514db9f39
hcp1152368178 <1152368178@qq.com>
2019-08-06 20:24:20 +0800

    05事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -20,41 +20,97 @@ Page({
       {id:6,name:'小黑',age:22}
     ],
     lists:[],
-    counter:0
+    counter:0,
+    titles:['衣服','裤子','鞋子']
   },
+  handleTouchStart(){
+    console.log('handleTouchStart')
+  },
+  handleTouchMove() {
+    console.log('handleTouchMove')
+  },
+  handleTouchEnd() {
+    console.log('handleTouchEnd')
+  },
+  handleTap() {
+    console.log('handleTap')
+  },
+  handleLongpress() {
+    console.log('handleLongpress')
+  },
+  handleEventClick(event){
+    console.log('-----',event)
+  },
+  handleTouchEndEvent(event){
+    console.log('+++++++',event)
+  },
+  handleOut(event) {
+    console.log('Outer:',event)
+  },
+  handleInner(event){
+    console.log('inner:',event)
+  },
+  handleItemClick(event){
+    console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title,index)
+  },
+  // ---------事件冒泡和捕获
+  handleCaptureView1(){
+    console.log('handleBindView1')
+  },
+  handleCaptureView2() {
+    console.log('handleBindView2')
+  },
+  handleCaptureView3() {
+    console.log('handleBindView3')
+  },
+
+  handleBindView1() {
+    console.log('handleBindView1')
+  },
+  handleBindView2() {
+    console.log('handleBindView2')
+  },
+  handleBindView3() {
+    console.log('handleBindView3')
+  },
+  
   
 
   // 1.监听页面的生面周期
   // 页面被加载出来
   onLoad(){
-    console.log('onLoad')
-    wx.request({
-      // 网络请求
-      url: 'http://123.207.32.32:8000/recommend',
-      success:(res)=>{
-        console.log(res)
-        const data = res.data.data.list;
-        this.setData({
-          lists: data
-        })
-      }
-    })
+    // console.log('onLoad')
+    // wx.request({
+    //   // 网络请求
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:(res)=>{
+    //     console.log(res)
+    //     const data = res.data.data.list;
+    //     this.setData({
+    //       lists: data
+    //     })
+    //   }
+    // })
   },
   // 页面显示出来
   onShow() {
-    console.log('onShow')
+    // console.log('onShow')
   },
   // 页面初次渲染完成时回调
   onReady() {
-    console.log('onReady')
+    // console.log('onReady')
   },
   // 当页面隐藏时
   onHide() {
-    console.log('onHide')
+    // console.log('onHide')
   },
   // 监听页面卸载
   onUnload() {
-    console.log('onUnload')
+    // console.log('onUnload')
   },
 
 // 3.监听wxml相关的一些事件
