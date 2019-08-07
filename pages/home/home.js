76c37a570b292e995c8d815815776cccd413d8d3
hcp1152368178 <1152368178@qq.com>
2019-08-07 19:17:29 +0800

    07网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,91 +1,105 @@
-// getApp()获取APP()产生的实例对象
-// const app = getApp();
-
-// const name = app.globalDate.name;
-// const age = app.globalDate.age;
-
+import request from '../../service/network.js'
 // 注册页面
 // 每个页面也有生命周期
 Page({
   // 2.初始化数据
   data:{
-    name:'xiaobai',
-    age:18,
-    students:[
-      { id: 1, name:'超强华',age:12},
-      {id:2,name:'超强光',age:15},
-      {id:3,name:'张三',age:18},
-      {id:4,name:'李四',age:72},
-      {id:5,name:'小白',age:19},
-      {id:6,name:'小黑',age:22}
-    ],
-    lists:[],
-    counter:0
+    name:'xiaobai'
   },
-  
-
   // 1.监听页面的生面周期
   // 页面被加载出来
-  onLoad(){
-    console.log('onLoad')
-    wx.request({
-      // 网络请求
-      url: 'http://123.207.32.32:8000/recommend',
-      success:(res)=>{
-        console.log(res)
-        const data = res.data.data.list;
-        this.setData({
-          lists: data
-        })
-      }
-    })
-  },
-  // 页面显示出来
-  onShow() {
-    console.log('onShow')
-  },
-  // 页面初次渲染完成时回调
-  onReady() {
-    console.log('onReady')
-  },
-  // 当页面隐藏时
-  onHide() {
-    console.log('onHide')
-  },
-  // 监听页面卸载
-  onUnload() {
-    console.log('onUnload')
-  },
+  onLoad:function(options){
+    // 1.原生发送网络请求
+    this.get_data_origin()
 
-// 3.监听wxml相关的一些事件
-  handleBtnClick() {
-    console.log('点了+号')
-    // this.setData()
-    this.setData({
-      counter: this.data.counter + 1
-    })
-  },
-  handleSubtraction() { 
-    console.log('点了-号')
-    this.setData({
-      counter: this.data.counter - 1
-    })
-  },
-  handleGetUserInfo(event) {
-    console.log(event)
-  },
+    // 2.使用封装的request发送网络请求
+    // promise最大的好处就是可以防止回调地狱
+   request({
+     url:'http://123.207.32.32:8000/recommend'
+   })
+   .then(res=>{
+     console.log(res)
+   }).catch(err=>{
+     console.log(err)
+   })
 
-  // 4.其它事件
-  // 监听页面的滚动
-  onPageScroll(obj){
-    console.log(obj)
+
+
+    // 发送网络请求
+    // 发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:(res)=>{
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2.get 请求，但携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2.post 请求，携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method:'post',
+    //   data: {
+    //    name:'xiaobai',
+    //    age:18
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   },
+    //   fail:function(err){
+    //     console.log(err)
+    //   }
+    // })
+
   },
-  // 监听页面滚动到底部
-  onReachBottom(){
-    console.log('页面已经滚动到底部')
-  },
-  // 下拉刷新
-  onPullDownRefresh(){
-    console.log('下拉刷新中....')
+  get_data_origin(){
+    // 发送网络请求
+    // 发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:(res)=>{
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2.get 请求，但携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2.post 请求，携带参数
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method:'post',
+    //   data: {
+    //    name:'xiaobai',
+    //    age:18
+    //   },
+    //   success: (res) => {
+    //     console.log(res)
+    //   },
+    //   fail:function(err){
+    //     console.log(err)
+    //   }
+    // })
+
   }
 })
